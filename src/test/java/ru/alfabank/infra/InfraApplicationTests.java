0c990c15812ba9def020ad5258df800a9217d4b1
Ivan Pohodnya <pohodnyaivan@gmail.com>
2018-12-08 00:41:50 +0300

    timed test

--- src/test/java/ru/alfabank/infra/InfraApplicationTests.java
+++ src/test/java/ru/alfabank/infra/InfraApplicationTests.java
@@ -1,6 +1,6 @@
 package ru.alfabank.infra;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -12,9 +12,10 @@ public class InfraApplicationTests {
 	@Test
 	public void contextLoads() {
 		try {
+			Thread.sleep(100_000);
 			var runtimeException = new RuntimeException();
 			throw runtimeException;
-		} catch (RuntimeException e) {
+		} catch (RuntimeException | InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
